7a8fec0ab64e1170209c698ec0a7a5cf2923695c
Artem Nikonov <nikonov.artem@220-volt.ru>
2015-12-11 16:19:42 +0300

    Added optional arguments into manager

--- importer/management/commands/import.py
+++ importer/management/commands/import.py
@@ -23,6 +23,9 @@ class Command(BaseCommand):
         parser.add_argument('parser', help='current parser')
         parser.add_argument('source', help='path to file')
 
+        parser.add_argument('--events', nargs='*', help='list of events to be updated')
+        parser.add_argument('--places', nargs='*', help='list of places to be updated')
+
     def handle(self, *args, **options):
         try:
             # TODO чота треш какой-то. Переделать бы
@@ -36,7 +39,19 @@ class Command(BaseCommand):
             parser = parser_class(options['source'])
             logger.info('Parse data. Done.')
 
-            Mapper(parser.data)
+            data = parser.data
+
+            if options['events']:
+                events_list = map(int, options['events'])
+                data['events'] = [x for x in data['events'] if x['external_id'] in events_list]
+                data['schedule'] = [x for x in data['schedule'] if x['event'] in events_list]
+
+            if options['places']:
+                places_list = map(int, options['places'])
+                data['places'] = [x for x in data['places'] if x['external_id'] in places_list]
+                data['schedule'] = [x for x in data['schedule'] if x['place'] in places_list]
+
+            Mapper(data)
         except (ImportError, AttributeError):
             print 'Make sure that you are using the correct parser'
 
